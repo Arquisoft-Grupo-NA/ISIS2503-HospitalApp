f85cad1011822e7d69bb2380d2c98c51fbb48e9b
EL_MARTINEZS <sebastianmartinezarias@gmail.com>
2025-05-04 02:54:03 -0500

    .

--- MRI/forms.py
+++ MRI/forms.py
@@ -1,8 +1,5 @@
 from django import forms
 from .models import MRI
-from cliente.logic.cliente_logic import get_clientes
-from monitoring.crypto import decrypt
-
 class MRIForm(forms.ModelForm):
     class Meta:
         model = MRI
@@ -13,16 +10,3 @@ class MRIForm(forms.ModelForm):
             'hora': 'Hora',
             'descripcion': 'Descripción'
         }
-
-    def __init__(self, *args, **kwargs):
-        super(MRIForm, self).__init__(*args, **kwargs)
-        for cliente in self.fields['cliente'].queryset:
-            try:
-                cliente.name = decrypt(cliente.name)
-                print(cliente.name)
-                print("ola")
-            except Exception as e:
-                cliente.name = 'Desencriptación fallida'
-
-        print(self.fields['cliente'].queryset)
-        print("ola")
