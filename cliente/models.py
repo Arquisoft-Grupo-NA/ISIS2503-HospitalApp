21d5308e91f53bcbe7f4bbac865b31b372656de6
EL_MARTINEZS <sebastianmartinezarias@gmail.com>
2025-05-04 01:43:01 -0500

    maximo caracteres nombre

--- cliente/models.py
+++ cliente/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Cliente(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=120)
     name_hmac = models.CharField(max_length=64, blank=True)
 
     def __str__(self):
