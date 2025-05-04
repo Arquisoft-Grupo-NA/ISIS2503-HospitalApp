b8a7922ea0242b551e9402bfc91284cc7002c655
EL_MARTINEZS <sebastianmartinezarias@gmail.com>
2025-05-04 01:31:55 -0500

    aa

--- cliente/models.py
+++ cliente/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Cliente(models.Model):
     name = models.CharField(max_length=50)
-    name_hmac = models.CharField(blank=True)
+    name_hmac = models.CharField(max_length=64, blank=True)
 
     def __str__(self):
         return '{}'.format(self.name)
