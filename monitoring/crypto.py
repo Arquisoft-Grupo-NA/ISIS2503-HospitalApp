34dc5966a2562a775eec4aad23b64cc214a2801d
EL_MARTINEZS <sebastianmartinezarias@gmail.com>
2025-05-04 01:29:42 -0500

    correcion

--- monitoring/crypto.py
+++ monitoring/crypto.py
@@ -16,5 +16,5 @@ def gHMAC(value: str) -> str:
     return hmac.new(HMAC_SECRET, value.encode(), hashlib.sha256).hexdigest()
 
 def vHMAC(value: str, hmac_value: str) -> bool:
-    expected_hmac = generate_hmac(value)
+    expected_hmac = gHMAC(value)
     return hmac.compare_digest(expected_hmac, hmac_value)
