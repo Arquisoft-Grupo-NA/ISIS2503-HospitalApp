566916411a3ed11c24b36cdb3645f7000ae903fc
EL_MARTINEZS <sebastianmartinezarias@gmail.com>
2025-05-04 21:31:47 -0500

    sirvepls

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -148,7 +148,7 @@ if cloud_logging_handler:
 
 LOGIN_URL = "/login/auth0"
 LOGIN_REDIRECT_URL = "/"
-LOGOUT_REDIRECT_URL ="https://dev-7gjasd3m5ecgyzk7.us.auth0.com/v2/logout?returnTo=http%3A%2F%2F34.27.132.90"
+LOGOUT_REDIRECT_URL ="https://dev-7gjasd3m5ecgyzk7.us.auth0.com/v2/logout?returnTo=http%3A%2F%2F34.27.132.90:8000"
 SOCIAL_AUTH_TRAILING_SLASH = False # Remove end slash from routes
 SOCIAL_AUTH_AUTH0_DOMAIN = 'dev-7gjasd3m5ecgyzk7.us.auth0.com'
 SOCIAL_AUTH_AUTH0_KEY = 'HDMXN5FmEBR7PZYgsqpxu2X8lR1SDbyZ'
