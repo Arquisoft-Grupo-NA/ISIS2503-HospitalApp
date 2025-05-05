d6feda67d10333a5fc8cf5246ce6428a698930cb
EL_MARTINEZS <sebastianmartinezarias@gmail.com>
2025-05-04 20:14:20 -0500

    callback

--- monitoring/views.py
+++ monitoring/views.py
@@ -14,3 +14,6 @@ def mi_vista_de_prueba(request):
     logger.info("Accediendo a la vista de prueba de error desde monitoring/views.py")
     raise ValueError("Este es un error de prueba intencional desde monitoring/views.py.")
     return HttpResponse("OK")
+
+def callback_view(request):
+    return render(request, 'callback.html')
